b4b27d2596c8183874966c87f9d3da7787e93d11
Markus Emrich <Markus.Emrich@gmail.com>
2013-10-26 23:06:51 +0100

    added linechart pod

--- SmokeFree/SFDetailsViewController.m
+++ SmokeFree/SFDetailsViewController.m
@@ -6,6 +6,8 @@
 //
 //
 
+#import <ios-linechart/LineChartView.h>
+
 #import "SFDetailsViewController.h"
 
 @interface SFDetailsViewController ()
@@ -27,6 +29,8 @@
     [UIView animateWithDuration:0.4 delay:0.2 options:UIViewAnimationOptionCurveEaseOut animations:^{
         self.topView.frameY = 0;
     } completion:nil];
+    
+    [self addChart];
 }
 
 - (void)setDidReachTarget:(BOOL)didReachTarget;
@@ -48,6 +52,41 @@
         self.positiveImage.hidden = YES;
         self.negativeImage.hidden = NO;
     }
+}
+
+- (void)addChart;
+{
+    LineChartData *d = [LineChartData new];
+    d.xMin = 1;
+    d.xMax = 31;
+    d.title = @"The title for the legend";
+    d.color = [UIColor redColor];
+    d.itemCount = 10;
+    
+    NSMutableArray *vals = [NSMutableArray new];
+    for(NSUInteger i = 0; i < d.itemCount; ++i) {
+        [vals addObject:@((rand() / (float)RAND_MAX) * (31 - 1) + 1)];
+    }
+    [vals sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+        return [obj1 compare:obj2];
+    }];
+    d.getData = ^(NSUInteger item) {
+        float x = [vals[item] floatValue];
+        float y = powf(2, x / 7);
+        NSString *label1 = [NSString stringWithFormat:@"%d", item];
+        NSString *label2 = [NSString stringWithFormat:@"%f", y];
+        return [LineChartDataItem dataItemWithX:x y:y xLabel:label1 dataLabel:label2];
+    };
+    
+    LineChartView *chartView = [[LineChartView alloc] initWithFrame:CGRectMake(20, 350, 300, 300)];
+    chartView.yMin = 0;
+    chartView.yMax = powf(2, 31 / 7) + 0.5;
+    chartView.ySteps = @[@"0.0",
+                         [NSString stringWithFormat:@"%.02f", chartView.yMax / 2],
+                         [NSString stringWithFormat:@"%.02f", chartView.yMax]];
+    chartView.data = @[d];
+    
+    [self.view addSubview:chartView];
 }
 
 @end
