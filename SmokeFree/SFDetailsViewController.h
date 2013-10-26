e8e3d2b499a763685c3cccf640ae196ee50c7a46
Markus Emrich <Markus.Emrich@gmail.com>
2013-10-26 22:47:44 +0100

    added details animation / texts

--- SmokeFree/SFDetailsViewController.h
+++ SmokeFree/SFDetailsViewController.h
@@ -10,6 +10,11 @@
 
 @interface SFDetailsViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIView *topView;
+@property (weak, nonatomic) IBOutlet UIImageView *bgImage;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subTitleLabel;
+
 @property (nonatomic, assign) CGFloat value;
 
 @end
--- SmokeFree/SFDetailsViewController.m
+++ SmokeFree/SFDetailsViewController.m
@@ -19,13 +19,31 @@
     [super viewDidLoad];
     
     self.view.backgroundColor = [UIColor whiteColor];
+    self.bgImage.image = [[UIImage imageNamed:@"bg_details"] imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    [self setDidReachTarget:(self.value < 0)];
     
-    UIImageView *bg = [[UIImageView alloc] initWithImage:
-                       [[UIImage imageNamed:@"bg_details"]
-                        imageWithRenderingMode: UIImageRenderingModeAlwaysTemplate]];
-    [self.view addSubview:bg];
-    
-    bg.tintColor = (self.value < 0) ? [UIColor smokeFreeGreen] : [UIColor smokeFreeRed];
+    // animate in
+    self.topView.frameBottom = 0;
+    [UIView animateWithDuration:0.4 delay:0.2 options:UIViewAnimationOptionCurveEaseOut animations:^{
+        self.topView.frameY = 0;
+    } completion:nil];
+}
+
+- (void)setDidReachTarget:(BOOL)didReachTarget;
+{
+    if (didReachTarget) {
+        self.bgImage.tintColor = [UIColor smokeFreeGreen];
+        self.titleLabel.text = @"That's how we roll!";
+        self.subTitleLabel.text = @"You reached your goal today!";
+    } else {
+        self.bgImage.tintColor = [UIColor smokeFreeRed];
+        self.titleLabel.text = @"Come on!";
+        if (self.value < 1.0) {
+            self.subTitleLabel.text = @"So close… tomorrow you can do it!";
+        } else {
+            self.subTitleLabel.text = @"You can do better! You wanted this, no?";
+        }
+    }
 }
 
 @end
